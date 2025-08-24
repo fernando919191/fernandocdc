f926d69e5afa4ecf3830ec4bb78e0e34c751cf14
fernando919191 <fer982756@gmail.com>
2025-08-24 16:51:31 -0600

    Create bin.js

---
+++ comandos/bin.js
@@ -0,0 +1,43 @@
+import fetch from "node-fetch";
+import { PREFIX } from "../funcionamiento/config.js";
+
+export default {
+  name: "bin",
+  description: "Revisa información de un BIN",
+  prefix: PREFIX,
+  async execute(bot, msg) {
+    const parts = msg.text.split(" ");
+    const bin_input = parts[1];
+
+    if (!bin_input || !/^\d{6,}$/.test(bin_input)) {
+      return bot.sendMessage(msg.chat.id, "❌ Debes colocar /bin seguido de un BIN numérico de al menos 6 dígitos.");
+    }
+
+    try {
+      const res = await fetch(`https://data.handyapi.com/bin/${bin_input}`);
+      const api = await res.json();
+
+      if (api.Status === "SUCCESS") {
+        const paisNombre = api.Country?.Name || "Desconocido";
+        const marca = api.Scheme || "Desconocido";
+        const tipo = api.Type || "Desconocido";
+        const nivel = api.CardTier || "Desconocido";
+        const banco = api.Issuer || "Desconocido";
+
+        bot.sendMessage(msg.chat.id, `
+🔹 Información solicitada:
+Bin: ${bin_input}
+Nivel: ${nivel}
+Tipo: ${tipo}
+Marca: ${marca}
+País: ${paisNombre}
+Banco: ${banco}
+        `);
+      } else {
+        bot.sendMessage(msg.chat.id, "❌ Por favor, ingresa un BIN válido.");
+      }
+    } catch (e) {
+      bot.sendMessage(msg.chat.id, `❌ Error al conectar con la API: ${e.message}`);
+    }
+  }
+};
